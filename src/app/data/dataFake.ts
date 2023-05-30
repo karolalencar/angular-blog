74479594963c627e406fd9894fd4d0442db8c116
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2023-05-30 10:40:48 -0300

    more dinamic implementation

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+	{
+		"id" : 1,
+		"photo": "aa",
+		"title": "Angular",
+		"description": "Olá"
+	},
+	{
+		"id" : 2,
+		"photo": "aa",
+		"title": "Angular",
+		"description": "Olá"
+	}
+]
